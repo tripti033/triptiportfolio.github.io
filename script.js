52e8e01d0038643d4af09f00a29bc6060b49e768
Tripti Verma <107789391+tripti033@users.noreply.github.com>
2023-01-09 20:26:29 +0530

    resume button

--- script.js
+++ script.js
@@ -61,9 +61,9 @@ menu_ele.forEach(menu__item => {
 // console.log(progessPr)
 
 
-// document.querySelector('#contact-form').addEventListener('submit', (e) => {
-//   e.preventDefault();
-//   e.target.elements.name.value = '';
-//   e.target.elements.email.value = '';
-//   e.target.elements.message.value = '';
+//document.querySelector('#contact-form').addEventListener('submit', (e) => {
+//e.preventDefault();
+// e.target.elements.name.value = '';
+// value = '';
+// value = '';
 // });
